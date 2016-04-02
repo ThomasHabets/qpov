61154a428cda604e0f8a788a8e45ac3a7699fc20
Thomas Habets <thomas@habets.se>
2016-04-02 15:51:04 +0100

    dscheduler/webui: Show lease ID

--- cmd/dscheduler/webui/tmpl_root.go
+++ cmd/dscheduler/webui/tmpl_root.go
@@ -80,6 +80,7 @@ tr:nth-child(odd) {
 <table>
 <tr>
   <th>Order</th>
+  <th>Lease</th>
   <th>Created</th>
   <th>Done</th>
   <th>Time</th>
@@ -87,18 +88,19 @@ tr:nth-child(odd) {
 <!--  <th>Package</th> -->
   <th>File</th>
 <!--  <th>Args</th> -->
-  <th>Details</th>
+  <th>Client</th>
 </tr>
 {{range .DoneLeases}}
 <tr>
   <td nowrap class="fixed">{{.OrderId}}</td>
+  <td nowrap class="fixed"><a href="{{$root.Root}}/lease/{{.LeaseId}}">{{.LeaseId}}</a></td>
   <td nowrap>{{.CreatedMs|fmsdate "2006-01-02 15:04"}}</td>
   <td nowrap>{{.UpdatedMs|fmsdate "2006-01-02 15:04"}}</td>
   <td nowrap>{{.CreatedMs|fmssub .UpdatedMs}}</td>
   <td nowrap><a href="{{$root.Root}}/image/{{.LeaseId}}">Image</a></td>
 <!--  <td nowrap>{{.Order.Package|fileonly}}</td> -->
   <td nowrap>{{.Order.File}}</td>
-  <td nowrap><a href="{{$root.Root}}/lease/{{.LeaseId}}">Details</a></td>
+  <td nowrap>{{.Address}}</td>
 <!--  <td nowrap>{{.Order.Args}}</td> -->
 </tr>
 {{end}}
