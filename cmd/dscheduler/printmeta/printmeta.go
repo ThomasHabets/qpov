1ca48503c71fa42a7c0c156712e6f0159a37d247
Thomas Habets <thomas@habets.se>
2016-04-02 15:23:24 +0100

    dscheduler: Add printmeta tool.

---
+++ cmd/dscheduler/printmeta/printmeta.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/golang/protobuf/proto"
+
+	pb "github.com/ThomasHabets/qpov/dist/qpov"
+)
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	fz, err := gzip.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(fz)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var p pb.RenderingMetadata
+	if err := proto.Unmarshal(b, &p); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(proto.MarshalTextString(&p))
+}
