34357dbf8f130d5fdbf783441f2feb0259415377
Thomas Habets <thomas@habets.se>
2016-04-09 11:51:53 +0100

    dscheduler/webui: Make web page a bit prettier.

--- cmd/dscheduler/webui/tmpl_root.go
+++ cmd/dscheduler/webui/tmpl_root.go
@@ -53,95 +53,111 @@ td {
 tr:nth-child(odd) {
   background: #EEE
 }
+#nav {
+  font-size: 24px;
+  height: 36px;
+  width: 100%;
+  color: black;
+  background-color: lightblue;
+}
+#gbuttons {
+  float: right;
+  display: inline-block;
+}
 </style>
   </head>
-  <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
-  <a href="#" onclick="signOut();">Sign out</a>
+  <div id="nav">
+    QPov
+    <div id="gbuttons">
+      <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    </div>
+  </div>
   <body>
-<h1>QPov</h1>
 
-{{if .Errors}}
-  <h2>Errors while rendering this page:</h2>
-  <ul>
-    {{range .Errors}}
-      {{.}}
+    {{if .Errors}}
+      <h2>Errors while rendering this page:</h2>
+      <ul>
+        {{range .Errors}}
+          <li>{{.}}</li>
+        {{end}}
+      </ul>
     {{end}}
-  </ul>
-{{end}}
 
-<h2>Scheduler stats</h2>
-<table>
-<tr><th colspan="2">Orders</th></tr>
-<tr><th>Total</th><td>{{.Stats.SchedulingStats.Orders}}</td></tr>
-<tr><th>Active</th><td>{{.Stats.SchedulingStats.ActiveOrders}}</td></tr>
-<tr><th>Done</th><td>{{.Stats.SchedulingStats.DoneOrders}}</td></tr>
-<tr><th>Unstarted</th><td>{{.UnstartedOrders}}</td></tr>
-<tr><th colspan="2">Leases</th></tr>
-<tr><th>Total</th><td>{{.Stats.SchedulingStats.Leases}}</td></tr>
-<tr><th>Active</th><td>{{.Stats.SchedulingStats.ActiveLeases}}</td></tr>
-<tr><th>Done</th><td>{{.Stats.SchedulingStats.DoneLeases}}</td></tr>
-</table>
+    <h2>Scheduler stats</h2>
+    {{if .Stats.SchedulingStats}}
+      <table>
+        <tr><th colspan="2">Orders</th></tr>
+        <tr><th>Total</th><td>{{.Stats.SchedulingStats.Orders}}</td></tr>
+        <tr><th>Active</th><td>{{.Stats.SchedulingStats.ActiveOrders}}</td></tr>
+        <tr><th>Done</th><td>{{.Stats.SchedulingStats.DoneOrders}}</td></tr>
+        <tr><th>Unstarted</th><td>{{.UnstartedOrders}}</td></tr>
+        <tr><th colspan="2">Leases</th></tr>
+        <tr><th>Total</th><td>{{.Stats.SchedulingStats.Leases}}</td></tr>
+        <tr><th>Active</th><td>{{.Stats.SchedulingStats.ActiveLeases}}</td></tr>
+        <tr><th>Done</th><td>{{.Stats.SchedulingStats.DoneLeases}}</td></tr>
+      </table>
+    {{end}}
 
-<h2>Active leases</h2>
-<table>
-<tr>
-  <th>Order</th>
-  <th>Lease</th>
-  <th>Created</th>
-  <th>Lifetime</th>
-  <th>Updated</th>
-  <th>Expires</th>
-<!--  <th>Package</th> -->
-  <th>File</th>
-  <th>Client</th>
-</tr>
-{{range .Leases}}
-<tr>
-  <td nowrap class="fixed">{{.OrderId}}</td>
-  <td nowrap class="fixed"><a href="{{$root.Root}}/lease/{{.LeaseId}}">{{.LeaseId}}</a></td>
-  <td nowrap>{{.CreatedMs|fmsdate "2006-01-02 15:04"}}</td>
-  <td nowrap>{{.CreatedMs|fmssince}}</td>
-  <td nowrap>{{.UpdatedMs|fmssince}}</td>
-  <td nowrap>{{.ExpiresMs|fmsuntil}}</td>
-<!--  <td nowrap>{{.Order.Package|fileonly}}</td> -->
-  <td nowrap>{{.Order.File}}</td>
-  <td nowrap>{{.Address}}</td>
-</tr>
-{{end}}
-</table>
+    <h2>Active leases</h2>
+    <table>
+      <tr>
+        <th>Order</th>
+        <th>Lease</th>
+        <th>Created</th>
+        <th>Lifetime</th>
+        <th>Updated</th>
+        <th>Expires</th>
+        <!--  <th>Package</th> -->
+        <th>File</th>
+        <th>Client</th>
+      </tr>
+      {{range .Leases}}
+        <tr>
+          <td nowrap class="fixed">{{.OrderId}}</td>
+          <td nowrap class="fixed"><a href="{{$root.Root}}/lease/{{.LeaseId}}">{{.LeaseId}}</a></td>
+          <td nowrap>{{.CreatedMs|fmsdate "2006-01-02 15:04"}}</td>
+          <td nowrap>{{.CreatedMs|fmssince}}</td>
+          <td nowrap>{{.UpdatedMs|fmssince}}</td>
+          <td nowrap>{{.ExpiresMs|fmsuntil}}</td>
+          <!--  <td nowrap>{{.Order.Package|fileonly}}</td> -->
+          <td nowrap>{{.Order.File}}</td>
+          <td nowrap>{{.Address}}</td>
+        </tr>
+      {{end}}
+    </table>
 
-<h2>Finished</h2>
-<table>
-<tr>
-  <th>Order</th>
-  <th>Lease</th>
-  <th>Created</th>
-  <th>Done</th>
-  <th>Time</th>
-  <th>Image</th>
-<!--  <th>Package</th> -->
-  <th>File</th>
-<!--  <th>Args</th> -->
-  <th>Client</th>
-</tr>
-{{range .DoneLeases}}
-<tr>
-  <td nowrap class="fixed">{{.OrderId}}</td>
-  <td nowrap class="fixed"><a href="{{$root.Root}}/lease/{{.LeaseId}}">{{.LeaseId}}</a></td>
-  <td nowrap>{{.CreatedMs|fmsdate "2006-01-02 15:04"}}</td>
-  <td nowrap>{{.UpdatedMs|fmsdate "2006-01-02 15:04"}}</td>
-  <td nowrap>{{.CreatedMs|fmssub .UpdatedMs}}</td>
-  <td nowrap><a href="{{$root.Root}}/image/{{.LeaseId}}">Image</a></td>
-<!--  <td nowrap>{{.Order.Package|fileonly}}</td> -->
-  <td nowrap>{{.Order.File}}</td>
-  <td nowrap>{{.Address}}</td>
-<!--  <td nowrap>{{.Order.Args}}</td> -->
-</tr>
-{{end}}
-</table>
+    <h2>Finished</h2>
+    <table>
+      <tr>
+        <th>Order</th>
+        <th>Lease</th>
+        <th>Created</th>
+        <th>Done</th>
+        <th>Time</th>
+        <th>Image</th>
+        <!--  <th>Package</th> -->
+        <th>File</th>
+        <!--  <th>Args</th> -->
+        <th>Client</th>
+      </tr>
+      {{range .DoneLeases}}
+        <tr>
+          <td nowrap class="fixed">{{.OrderId}}</td>
+          <td nowrap class="fixed"><a href="{{$root.Root}}/lease/{{.LeaseId}}">{{.LeaseId}}</a></td>
+          <td nowrap>{{.CreatedMs|fmsdate "2006-01-02 15:04"}}</td>
+          <td nowrap>{{.UpdatedMs|fmsdate "2006-01-02 15:04"}}</td>
+          <td nowrap>{{.CreatedMs|fmssub .UpdatedMs}}</td>
+          <td nowrap><a href="{{$root.Root}}/image/{{.LeaseId}}">Image</a></td>
+          <!--  <td nowrap>{{.Order.Package|fileonly}}</td> -->
+          <td nowrap>{{.Order.File}}</td>
+          <td nowrap>{{.Address}}</td>
+          <!--  <td nowrap>{{.Order.Args}}</td> -->
+        </tr>
+      {{end}}
+    </table>
 
-<hr>
-Page server time: {{.PageTime}}
+    <hr>
+    Page server time: {{.PageTime}}
   </body>
 </html>
 `
