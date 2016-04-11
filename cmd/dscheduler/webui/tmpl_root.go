eba20bc7a6a51b413307401f14fbad5f260494da
Thomas Habets <thomas@habets.se>
2016-04-11 20:18:15 +0100

    dscheduler/webui: Add sign-out button

--- cmd/dscheduler/webui/tmpl_root.go
+++ cmd/dscheduler/webui/tmpl_root.go
@@ -13,6 +13,8 @@ const rootTmpl = `
     function signOut() {
       var auth2 = gapi.auth2.getAuthInstance();
       auth2.signOut().then(function () {});
+      document.cookie = "jwt=;max-age=0";
+      location.reload();
     }
     function onSignIn(googleUser) {
       var profile = googleUser.getBasicProfile();
@@ -20,8 +22,13 @@ const rootTmpl = `
       if (img != undefined) {
         document.getElementById("profile-img").innerHTML = "<img src='"+img+"'>";
       }
+      document.getElementById("sign-out").style.display = "inline";
       document.getElementById("profile-email").innerHTML = profile.getEmail();
-      document.cookie = "jwt=" + googleUser.getAuthResponse().id_token;
+      document.cookie = "jwt=" + googleUser.getAuthResponse().id_token + ";secure";
+      // TODO: do ajax call to exchange for qpov cookie that is httponly, and
+      // delete the jwt cookie. The reload can then happen when new login, and
+      // do a refresh. Also the jwt cookie is pretty big.
+      // location.reload();
     };
   </script>
 <style>
@@ -68,6 +75,9 @@ tr:nth-child(odd) {
   height: 36px;
   width: 36px;
 }
+.top-button {
+  float: right;
+}
 </style>
   </head>
   <div id="nav">
@@ -75,6 +85,7 @@ tr:nth-child(odd) {
     <div id="gbuttons">
       <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
     </div>
+    <span class="top-button" id="sign-out" style="display: none"><a href="#" onclick="signOut();">Sign out</a></span>
     <span id="profile-img"></span>
     <span id="profile-email"></span>
   </div>
