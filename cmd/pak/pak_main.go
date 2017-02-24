fa2a078058dcdb76d35ea081f9160d80827b5f63
Thomas Habets <thomas@habets.se>
2017-02-24 21:50:05 +0000

    pak: print usage to stdout

--- cmd/pak/pak_main.go
+++ cmd/pak/pak_main.go
@@ -32,7 +32,7 @@ import (
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s [options] <pakfiles> command [command args...]\n", os.Args[0])
+	fmt.Fprintf(os.Stdout, "Usage: %s [options] <pakfiles> command [command args...]\n", os.Args[0])
 	flag.PrintDefaults()
 }
 
