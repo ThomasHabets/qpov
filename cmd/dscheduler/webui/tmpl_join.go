602011c9678472162174aa8cc1e9b21169e242e2
Thomas Habets <habets@google.com>
2020-12-18 11:12:15 +0100

    dscheduler/webui: Fix link to drender binary

--- cmd/dscheduler/webui/tmpl_join.go
+++ cmd/dscheduler/webui/tmpl_join.go
@@ -7,7 +7,7 @@ const joinTmpl = `
 <pre>
 # Its runs in the foreground, so you may want to do "screen -S qpov" first.
 sudo apt-get install povray povray-includes schedtool
-wget -O drender https://cdn.habets.se/tmp/drender-$(uname -m)
+wget -O drender https://cdn.habets.se/tmp/drender-$(uname -s)-$(uname -m)
 chmod +x drender
 mkdir wd
 ./drender -scheduler=qpov.retrofitta.se:9999 -wd=wd
