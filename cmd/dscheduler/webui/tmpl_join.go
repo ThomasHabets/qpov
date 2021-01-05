5372e08708aada739c4b35ee3c3a13032b5190e7
Thomas Habets <thomas@habets.se>
2021-01-05 23:52:17 +0100

    webui: Drop needless args in instructions

--- cmd/dscheduler/webui/tmpl_join.go
+++ cmd/dscheduler/webui/tmpl_join.go
@@ -10,7 +10,7 @@ sudo apt-get install povray povray-includes schedtool
 wget -O drender https://cdn.habets.se/tmp/drender-$(uname -s)-$(uname -m)
 chmod +x drender
 mkdir wd
-./drender -scheduler=qpov.retrofitta.se:9999 -wd=wd
+./drender
 </pre>
 <h2>Docker image</h2>
 Can be found <a href="https://hub.docker.com/r/thomashabets/qpov/">here</a>.
