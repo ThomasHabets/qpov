e81ce8108a93075dee40bc7da3a3b70131abbdcd
Thomas Habets <thomas@habets.se>
2026-06-11 10:51:56 +0100

    drender: fix GCE detection

--- cmd/drender/drender.go
+++ cmd/drender/drender.go
@@ -222,9 +222,11 @@ func getCloud() (string, string) {
 			defer resp.Body.Close()
 			b, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				re := regexp.MustCompile(`.*/`)
-				return googleCloud, re.ReplaceAllString(string(b), "")
-			}
+                                log.Debugf("GCE read failed: %v", err)
+                        } else {
+                                re := regexp.MustCompile(`.*/`)
+                                return googleCloud, re.ReplaceAllString(string(b), "")
+                        }
 		}
 	}
 
